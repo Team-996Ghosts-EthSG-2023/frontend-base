4f07c87db2b677b5001c67a2b66b787a92fb10d9
keanecodes <keanecodes@gmail.com>
2023-09-12 06:32:32 +0800

    just a static task

--- src/pages/App/Campaigns/Task/index.tsx
+++ src/pages/App/Campaigns/Task/index.tsx
@@ -2,11 +2,27 @@ import React from 'react'
 import { styled } from 'styled-components'
 
 export const Task = (props) => {
+    const descUrl = new URL('./1.jpg', import.meta.url).href
+    const submissionUrl = new URL('./2.jpg', import.meta.url).href
   return(
-    <Container>Task</Container>
+    <Container>
+        <TaskDescription src={descUrl}/>
+        <Submission src={submissionUrl}/>
+    </Container>
    )
   }
 
-  const Container = styled.div`
-    color: white;
-  `
+const TaskDescription = styled.img`
+  width: 70%;
+  margin: 0 auto;
+`
+const Submission = styled.img`
+  width: 70%;
+  margin: 0 auto;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: column;
+  overflow: scroll;
+`
